55174e88c5e8ed541380fbdf00c211d39ca37d18
Sanshiro Yoshida <halcat0x15a@gmail.com>
2019-03-30 01:47:58 +0900

    uniform variance

--- src/main/scala/kits/eff/Arrs.scala
+++ src/main/scala/kits/eff/Arrs.scala
@@ -16,7 +16,7 @@ sealed abstract class Arrs[-R, A, B] extends Product with Serializable {
           f(value) match {
             case Eff.Pure(v) => loop(v, r)
             case Eff.Impure(u, k) =>
-              Eff.Impure(u.extend[R], k ++ r)
+              Eff.Impure(u, k ++ r)
           }
         case Arrs.Node(Arrs.Node(ll, lr), r) =>
           loop(value, Arrs.Node(ll, Arrs.Node(lr, r)))
@@ -26,7 +26,7 @@ sealed abstract class Arrs[-R, A, B] extends Product with Serializable {
 }
 
 object Arrs {
-  case class Leaf[R, A, B](value: A => Eff[R, B]) extends Arrs[R, A, B]
+  case class Leaf[-R, A, B](value: A => Eff[R, B]) extends Arrs[R, A, B]
 
-  case class Node[R, A, B, C](left: Arrs[R, A, B], right: Arrs[R, B, C]) extends Arrs[R, A, C]
+  case class Node[-R, A, B, C](left: Arrs[R, A, B], right: Arrs[R, B, C]) extends Arrs[R, A, C]
 }
--- src/main/scala/kits/eff/Eff.scala
+++ src/main/scala/kits/eff/Eff.scala
@@ -2,8 +2,8 @@ package kits.eff
 
 trait Fx[A] extends Any
 
-case class Union[R, A](tag: Manifest[_], value: Fx[_]) {
-  def decomp[F, S](implicit F: Manifest[F]): Either[Union[S, A], F with Fx[A]] =
+case class Union[-R, A](tag: Manifest[_], value: Fx[_]) {
+  def decomp[F, S](implicit F: Manifest[F], ev: (F with S) <:< R): Either[Union[S, A], F with Fx[A]] =
     if (F.runtimeClass.isInstance(value) && tag <:< F)
       Right(value.asInstanceOf[F with Fx[A]])
     else
@@ -31,7 +31,7 @@ object Eff {
     def flatMap[S, B](f: A => Eff[S, B]): Eff[S, B] = f(value)
   }
 
-  case class Impure[R, A, B](union: Union[R, A], arrs: Arrs[R, A, B]) extends Eff[R, B] {
+  case class Impure[-R, A, B](union: Union[R, A], arrs: Arrs[R, A, B]) extends Eff[R, B] {
     def map[C](f: B => C): Eff[R, C] = Impure(union, arrs :+ (x => Pure(f(x))))
     def flatMap[S, C](f: B => Eff[S, C]): Eff[R with S, C] = Impure(union.extend[S], arrs :+ f)
   }
