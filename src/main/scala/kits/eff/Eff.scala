8fbef340e64415289b9550bbbddc05ab9e314806
Sanshiro Yoshida <halcat0x15a@gmail.com>
2019-04-12 13:30:31 +0900

    replace mapN with zipWith

--- src/main/scala/kits/eff/Eff.scala
+++ src/main/scala/kits/eff/Eff.scala
@@ -39,6 +39,15 @@ sealed abstract class Eff[-R, +A] extends Product with Serializable {
       case Eff.Impure(u, k) =>
         Eff.Impure(u.extend[S], k :+ f)
     }
+
+  def flatten[S, B](implicit ev: A <:< Eff[S, B]): Eff[R with S, B] =
+    flatMap(ev)
+
+  def zipWith[S, B, C](that: Eff[S, B])(f: (A, B) => C): Eff[R with S, C] =
+    ap(that.map(b => f(_, b)))
+
+  def zip[S, B](that: Eff[S, B]): Eff[R with S, (A, B)] =
+    zipWith(that)((_, _))
 }
 
 object Eff {
@@ -49,16 +58,8 @@ object Eff {
       case Pure(a) => a
     }
 
-  def map[R0, R1, A0, A1, B](ra0: Eff[R0, A0], ra1: Eff[R1, A1])(f: (A0, A1) => B): Eff[R0 with R1, B] = ra0.ap(ra1.map(a1 => f(_, a1)))
-
-  def map[R0, R1, R2, A0, A1, A2, B](ra0: Eff[R0, A0], ra1: Eff[R1, A1], ra2: Eff[R2, A2])(f: (A0, A1, A2) => B): Eff[R0 with R1 with R2, B] = ra0.ap(map(ra1, ra2)((a1, a2) => f(_, a1, a2)))
-
-  def map[R0, R1, R2, R3, A0, A1, A2, A3, B](ra0: Eff[R0, A0], ra1: Eff[R1, A1], ra2: Eff[R2, A2], ra3: Eff[R3, A3])(f: (A0, A1, A2, A3) => B): Eff[R0 with R1 with R2 with R3, B] = ra0.ap(map(ra1, ra2, ra3)((a1, a2, a3) => f(_, a1, a2, a3)))
-
-  def map[R0, R1, R2, R3, R4, A0, A1, A2, A3, A4, B](ra0: Eff[R0, A0], ra1: Eff[R1, A1], ra2: Eff[R2, A2], ra3: Eff[R3, A3], ra4: Eff[R4, A4])(f: (A0, A1, A2, A3, A4) => B): Eff[R0 with R1 with R2 with R3 with R4, B] = ra0.ap(map(ra1, ra2, ra3, ra4)((a1, a2, a3, a4) => f(_, a1, a2, a3, a4)))
-
   def traverse[R, A, B, M[X] <: TraversableOnce[X]](ma: M[A])(f: A => Eff[R, B])(implicit cbf: CanBuildFrom[M[A], B, M[B]]): Eff[R, M[B]] =
-    ma.foldLeft(Pure(cbf()): Eff[R, Builder[B, M[B]]])((fmb, a) => map(fmb, f(a))((mb, b) => mb += b)).map(_.result)
+    ma.foldLeft(Pure(cbf()): Eff[R, Builder[B, M[B]]])((fmb, a) => fmb.zipWith(f(a))((mb, b) => mb += b)).map(_.result)
 
   def sequence[R, A, M[X] <: TraversableOnce[X]](ma: M[Eff[R, A]])(implicit cbf: CanBuildFrom[M[Eff[R, A]], A, M[A]]): Eff[R, M[A]] =
     traverse(ma)(a => a)
